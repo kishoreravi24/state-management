9bc9e93e31192d36ced479592b67fcb0c458420c
kishoreravi24 <kishoreravi24399@gmail.com>
2020-12-28 21:04:43 +0530

    Redux completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+ import React from 'react';
+ import {useSelector, useDispatch} from 'react-redux';
+ import {increment, decrement, log, log1 } from './actions/index';
 
 function App() {
+
+  const counter = useSelector(state => state.counter);
+  const logged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>
+      <br/>
+      <h1>counter: {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>&nbsp;<button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(log())}>show_val</button>
+      <button onClick={() => dispatch(log1())}>not show the val</button>
+      <br/>
+      {logged ? (
+        <h1>Important message</h1>
+      ) : "" }
     </div>
   );
 }
